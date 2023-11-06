ca7b9a739c1a15df29a764dcef6b04aeb46684a7
rjshbangoy <richardjoshuabangoy@gmail.com>
2023-11-07 01:05:35 +0800

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,6 +4,7 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/
 import { app } from '../firebase';
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, signOutUserFailure, signOutUserSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -116,15 +117,24 @@ export default function Profile() {
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <h1 className='text-3xl font-semibold text-center my-7'>
+        Profile
+      </h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*' />
         <img onClick={() => fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt="profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
-        <p className='text-sm self-center'> {fileUploadError ? (<span className='text-red-700'>Error Image upload (image must be less than 2mb)</span>) : filePerc > 0 && filePerc < 100 ? (<span className='text-slate-700'>Uploading {filePerc}%</span>) : filePerc === 100 ? (<span className='text-green-700'>Image successfully uploaded!</span>) : ""} </p>
+        <p className='text-sm self-center'>
+          {fileUploadError ? (<span className='text-red-700'>Error Image upload (image must be less than 2mb)</span>) : filePerc > 0 && filePerc < 100 ? (<span className='text-slate-700'>Uploading {filePerc}%</span>) : filePerc === 100 ? (<span className='text-green-700'>Image successfully uploaded!</span>) : ""}
+         </p>
         <input type="text" placeholder='username' defaultValue={currentUser.username} id='username' className='border p-3 rounded-lg' onChange={handleChange} />
         <input type="email" placeholder='email' defaultValue={currentUser.email} id='email' className='border p-3 rounded-lg' onChange={handleChange} />
         <input type="password" placeholder='password' id='password' className='border p-3 rounded-lg' />
-        <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>{ loading ? 'Loading...' : 'Update' }</button>
+        <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>
+          { loading ? 'Loading...' : 'Update' }
+        </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:oppacity-95' to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
 
       <div className="flex justify-between mt-5">
