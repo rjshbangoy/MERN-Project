16742ea5317d0b940a4db248fb83722fbd70a101
rjshbangoy <richardjoshuabangoy@gmail.com>
2023-11-27 12:39:15 +0800

    add show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -16,6 +16,7 @@ export default function Search() {
 
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
+  const [showMore, setShowMore] = useState(false);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -49,9 +50,17 @@ export default function Search() {
 
     const fetchListings = async () => {
       setLoading(true);
+      setShowMore(false);
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
+
+      if (data.length > 8) {
+        setShowMore(true);
+      } else {
+        setShowMore(false);
+      };
+
       setListings(data);
       setLoading(false);
     };
@@ -93,6 +102,22 @@ export default function Search() {
 
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
+  };
+
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+
+    if (data.length < 9) {
+      setShowMore(false);
+    };
+
+    setListings([...listings, ...data]);
   };
 
   return (
@@ -211,7 +236,12 @@ export default function Search() {
           {loading && (
             <p className='text-xl text-slate-700 text-center w-full'>Loading...</p>
           )}
-          {!loading && listings && listings.map((listing) => <ListingItem key={listing._id} listing={listing}/>)}
+          {!loading && listings && listings.map(
+            (listing) => <ListingItem key={listing._id} listing={listing}/>
+          )}
+          {showMore && (
+            <button onClick={onShowMoreClick} className='text-green-700 hover:underline p-7 text-center w-full'>Show more</button>
+          )}
         </div>
       </div>
     </div>
